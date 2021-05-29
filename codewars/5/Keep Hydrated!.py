87975a34af6e4cc67935f307bb90045373ecce41
filipwroblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-05-29 11:25:58 +0200

    Add files via upload

---
+++ codewars/5/Keep Hydrated!.py
@@ -0,0 +1,11 @@
+def litres(time):
+    return int(time * 0.5)
+
+
+litres(2)   # 1
+litres(1.4) # 0
+litres(12.3)    # 6
+litres(0.82)    # 0
+litres(11.8)    # 5
+litres(1787)    # 893
+litres(0)   # 0
