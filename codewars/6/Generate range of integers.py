3ae8f17905459e3b018090c848dc773fa7ab4e7b
filipwroblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-06-03 01:51:58 -0400

    Add files via upload

---
+++ codewars/6/Generate range of integers.py
@@ -0,0 +1,11 @@
+def generate_range(min, max, step):
+    integers=[]
+    for i in range(min, max+1, step):
+        integers.append(i)
+    return integers
+
+generate_range(1, 10, 1)    # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+generate_range(-10, 1, 1)   # [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1])
+generate_range(1, 15, 20)   # [1])
+generate_range(1, 7, 2) # [1, 3, 5, 7])
+generate_range(0, 20, 3)    # [0, 3, 6, 9, 12, 15, 18])
