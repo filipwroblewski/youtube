0d816107f24b8b80e383913173d52779167c6248
filipwroblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-06-04 01:26:52 -0400

    Add files via upload

---
+++ codewars/6/The Wide-Mouthed frog.py
@@ -0,0 +1,8 @@
+def mouth_size(animal): 
+       if animal.lower() == "alligator":
+              return "small"
+       return "wide"
+
+mouth_size("toucan")        # "wide")
+mouth_size("ant bear")      # "wide")
+mouth_size("alligator")     # "small")
