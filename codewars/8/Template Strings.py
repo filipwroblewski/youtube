a075cf6b90d7a3e0efdb17731341c7d78544fca2
filipwroblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-06-20 10:01:37 -0400

    Add files via upload

---
+++ codewars/8/Template Strings.py
@@ -0,0 +1,7 @@
+def temple_strings(obj, feature): 
+    return f'{obj} are {feature}'
+
+temple_strings("Animals","Good")     # 'Animals are Good
+temple_strings("Animals","Good")     # 'Animals are Good
+temple_strings("You","Special")      # 'You are Special
+temple_strings("lives","frozen")     # 'lives are frozen
