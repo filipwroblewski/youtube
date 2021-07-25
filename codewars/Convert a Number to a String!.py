6987b68664312ff12d0d86a511e3a88ef1a343a4
filipwroblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-07-25 14:19:39 +0200

    Add files via upload

---
+++ codewars/Convert a Number to a String!.py
@@ -0,0 +1,8 @@
+def number_to_string(num):
+    return str(num)
+
+number_to_string(67)    # '67'
+number_to_string(79585) # '79585'
+number_to_string(79585) # "79585"
+number_to_string(1+2)   # '3'
+number_to_string(1-2)   # '-1'
